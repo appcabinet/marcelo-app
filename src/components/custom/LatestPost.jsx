41451abba0b37f916aea08b4ed007146a697d326
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2024-08-20 11:14:17 -0400

    chore: tweaks

--- src/components/custom/LatestPost.jsx
+++ src/components/custom/LatestPost.jsx
@@ -6,11 +6,11 @@ const LatestPost = ({ frontmatter }) => {
     return (
         <>
             <H3 className={"mb-2 md:mb-4 text-md md:text-lg"}>Latest</H3>
-            <Link href={`/w/${frontmatter.slug}`} className={"w-full flex flex-col items-start mb-4 rounded-lg"}>
-                <h4 className={"text-[17px] md:text-lg font-medium text-neutral-800 leading-6 mb-1 md:mb-2"}>
+            <Link href={`/w/${frontmatter.slug}`} className={"w-full flex flex-col items-start mb-4 rounded-lg group"}>
+                <h4 className={"text-[17px] md:text-lg font-medium text-neutral-800 leading-6 mb-0.5 md:mb-0.5 group-hover:text-orange-400 group-hover:underline"}>
                     {frontmatter.title}
                 </h4>
-                <p className={"text-sm md:text-[16px] mb-3 text-neutral-400"}>
+                <p className={"text-sm md:text-[15px] mb-3 text-neutral-400"}>
                     {frontmatter.created} · {frontmatter.duration}
                 </p>
 
--- src/components/custom/PostList.jsx
+++ src/components/custom/PostList.jsx
@@ -13,13 +13,13 @@ export default function PostList({ posts }) {
             <ul className={"w-full ml-0.5"}>
                 {posts.map(p => (
                     <li key={p.id}
-                        className={"w-full text-neutral-500 mb-1 md:mb-1 flex justify-start content-center items-center"}>
+                        className={"w-full text-neutral-500 mb-1 md:mb-1.5 flex justify-start content-center items-center"}>
                         <div className={"w-[125px] mr-2 hidden md:block"}>
                             <span className={`text-neutral-400 font-light ${dmMono.className}`}>{p.created}</span>
                         </div>
                         <Dot className={"mr-0 md:mr-2 md:hidden"}/>
                         <Link href={`/w/${p.slug}`}
-                              className={"hover:underline hover:text-orange-500 w-full text-md md:text-lg"}>
+                              className={"hover:underline hover:text-orange-500 w-full text-md md:text-md"}>
                             {p.title}
                         </Link>
                     </li>
